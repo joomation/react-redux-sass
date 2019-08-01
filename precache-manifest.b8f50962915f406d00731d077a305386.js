13e1d3e40b4fee4f4e7ca04be471c6fa25356fed
USER <user@AD01329567-880.local>
2019-08-01 11:36:33 +0900

    Updates

---
+++ precache-manifest.b8f50962915f406d00731d077a305386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5698fb3e43c871c112b8c50095b67da7",
+    "url": "/react-redux-sass/index.html"
+  },
+  {
+    "revision": "752adf9e894ead94ad8d",
+    "url": "/react-redux-sass/static/css/main.b1ce6121.chunk.css"
+  },
+  {
+    "revision": "b2ac29830d6da4d894d1",
+    "url": "/react-redux-sass/static/js/2.5a07646b.chunk.js"
+  },
+  {
+    "revision": "78c3e68957f0db10b49f",
+    "url": "/react-redux-sass/static/js/3.bd471f66.chunk.js"
+  },
+  {
+    "revision": "08bf6382113a75a28aca",
+    "url": "/react-redux-sass/static/js/4.cd30d6f3.chunk.js"
+  },
+  {
+    "revision": "752adf9e894ead94ad8d",
+    "url": "/react-redux-sass/static/js/main.5dc5eaf5.chunk.js"
+  },
+  {
+    "revision": "41cbb2ed19a0787025bf",
+    "url": "/react-redux-sass/static/js/runtime~main.36e2c87b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-scss/precache-manifest.970310c39ee4f29b2003d59edc9d080f.js"
+  "/react-redux-sass/precache-manifest.b8f50962915f406d00731d077a305386.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-scss/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-sass/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
